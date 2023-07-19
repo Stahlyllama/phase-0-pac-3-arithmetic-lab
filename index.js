931d13d6075711fc93cf090039d491c46a1ef129
Kim Stahl <kim.stahl@gmail.com>
2023-07-19 09:44:52 -0500

    arithmetic functions completed

---
+++ index.js
@@ -0,0 +1,35 @@
+let a = a;
+let b = 10; 
+
+//1 
+function add(a, b){
+   return a + b;
+}
+//2
+function subtract(a, b){
+  return a - b;
+}
+//3
+function multiply(a, b){
+  return a * b;
+}
+//4
+function divide(a, b){ 
+  return a / b; 
+}
+//5
+function increment(a, b){
+  return a = ++ a ;
+}
+//6
+function decrement(a, b){
+  return a = -- a;
+}
+//7
+function makeInt(n){
+  return parseInt (n, 10);
+    }
+
+  function preserveDecimal(n){
+    return parseFloat(n);
+  }
